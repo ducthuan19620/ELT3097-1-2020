c5e3f5eb22b5ea53d2acd89867277393eb6451f0
aztung25102000 <aztung25102000@gmail.com>
2020-12-02 15:37:31 +0700

    update

---
+++ NguyenVanTung/Doulingo/app/src/main/java/DucThuan/duolingo/UI/activity/LessonCompletedActivity.java
@@ -0,0 +1,52 @@
+package DucThuan.duolingo.UI.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import DucThuan.duolingo.R;
+import DucThuan.duolingo.UI.tasks.MutipleChoice;
+import DucThuan.duolingo.Utils.ActivityNavigation;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LessonCompletedActivity extends AppCompatActivity {
+
+    @BindView(R.id.continue_button)
+    Button continue_button;
+
+    Context context;
+
+    ActivityNavigation activityNavigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson_completed);
+
+        ButterKnife.bind(this);
+
+        back();
+    }
+
+    private void back() {
+        continue_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LessonCompletedActivity.this, LessonListActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        finish();
+        super.onStop();
+    }
+}
